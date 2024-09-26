b1b5b88b055c7cf4f5f43e3cec8622baa416faba
ArunKanojia93 <arun@pyzalabs.com>
2024-09-26 12:09:03 +0530

    feat: added rate limiter

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,6 +1,7 @@
 import { ChatWrapper } from "@/components/ChatWrapper";
 import { ragChat } from "@/lib/rag-chat";
 import { redis } from "@/lib/redis";
+import { Ratelimit } from "@upstash/ratelimit";
 import { cookies } from "next/headers";
 
 interface Pageprops {
@@ -14,6 +15,11 @@ function reconstructUrl({ url }: { url: string[] }) {
   return decodedComponents.join("/");
 }
 
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+});
+
 const Page = async ({ params }: Pageprops) => {
   const sessionCookie = cookies().get("sessionId")?.value;
   const reconstructedUrl = reconstructUrl({
@@ -26,13 +32,23 @@ const Page = async ({ params }: Pageprops) => {
 
   const initialMessages = await ragChat.history.getMessages({ amount: 10, sessionId });
 
-  if (!isAlreadyIndexed) {
+  const { success } = await ratelimit.limit(sessionId);
+
+  if (!isAlreadyIndexed && success) {
     await ragChat.context.add({
       type: "html",
       source: reconstructedUrl,
     });
 
     await redis.sadd("indexed-urls", reconstructedUrl);
+  }
+
+  if (!success) {
+    return (
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className="text-2xl text-white">Too many requests! Please try again later.</p>
+      </div>
+    );
   }
 
   return <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />;
